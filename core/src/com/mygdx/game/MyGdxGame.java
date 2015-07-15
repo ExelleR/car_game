d7458325e85a2a15a3c83c3312820a41007730eb
ilya <elibrch@gmail.com>
2015-07-15 13:42:01 +0300

    Initial commit

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,33 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.FPSLogger;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	FPSLogger fpsLogger;
+	Vector2 planeVelocity= new Vector2();
+	Vector2 planePosition= new Vector2();
+	Vector2 planeDefaultPosition= new Vector2();
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		fpsLogger = new FPSLogger();
+		img = new Texture("car.bmp");
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+}
